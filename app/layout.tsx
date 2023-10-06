055ce7f13c393ecaba762438e35680cba69900ba
neIsonp <nelsonponte37@gmail.com>
2023-10-06 14:45:28 +0000

    created homepage to list all countries

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Nunito_Sans } from "next/font/google";
+import Image from "next/image";
 
 const nunitoSans = Nunito_Sans({ subsets: ["latin"] });
 
@@ -16,7 +17,22 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={nunitoSans.className}>{children}</body>
+      <body className={nunitoSans.className}>
+        <main className="bg-gray-100 min-h-screen flex flex-col items-center">
+          <nav className="w-full bg-white h-16 flex items-center justify-center">
+            <section className="container flex items-center gap-2">
+              <Image
+                src="/Logo.svg"
+                alt="application logo"
+                width={48}
+                height={48}
+              />
+              <h1 className="font-bold text-2xl">Countries list</h1>
+            </section>
+          </nav>
+          {children}
+        </main>
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,43 @@
 import Image from "next/image";
 
-export default function Home() {
-  return <h1>home</h1>;
+type Country = {
+  name: {
+    common: string;
+  };
+  flags: {
+    svg: string;
+    alt: string;
+  };
+};
+
+async function getCountries(): Promise<Country[]> {
+  const response = await fetch("https://restcountries.com/v3.1/all");
+  return response.json();
+}
+
+export default async function Home() {
+  const countries = await getCountries();
+
+  return (
+    <section className="grid grid-cols-5 w-full container gap-6 mt-16">
+      {countries.map((country) => (
+        <article
+          key={country.name.common}
+          className="h-64 min-w-full p-2 border-2 rounded-xl hover:border-indigo-200 hover:shadow-xl transition-all"
+        >
+          <div className="relative w-full h-40 p-2 overflow-hidden rounded-xl">
+            <Image
+              src={country.flags.svg}
+              alt={country.flags.alt}
+              fill
+              className="object-cover"
+            />
+          </div>
+          <h1 className="font-bold text-xl text-center mt-1">
+            {country.name.common}
+          </h1>
+        </article>
+      ))}
+    </section>
+  );
 }
